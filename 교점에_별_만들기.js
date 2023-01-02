afc689d609d825f6d693047bbad300bdcaafcb81
Sinkyo <yhko1988@gmail.com>
2023-01-03 08:33:16 +0900

    빛의 경로싸이클 완료
    - 그냥 접근 방법만 이해
    
    - 블록단위로 코테 관리

--- 교점에_별_만들기.js
+++ 교점에_별_만들기.js
@@ -1,101 +1,113 @@
-function getIntersectionFromLines([A, B, E], [C, D, F]) {
-  const x = (B * F - E * D) / (A * D - B * C);
-  const y = (E * C - A * F) / (A * D - B * C);
-  return x % 1 == 0 && y % 1 == 0 ? [x, y] : null;
-}
-
-function solution(line) {
-  const answer = [];
-
-  const intersectionPoints = {};
-
-  let upLimit = Number.MIN_SAFE_INTEGER;
-  let downLimit = Number.MAX_SAFE_INTEGER;
-  let leftLimit = Number.MAX_SAFE_INTEGER;
-  let rightLimit = Number.MIN_SAFE_INTEGER;
-
-  for (
-    let standardLineIdx = 0;
-    standardLineIdx < line.length;
-    standardLineIdx++
-  ) {
-    const standardLine = line[standardLineIdx];
-    for (
-      let compareLineIdx = standardLineIdx + 1;
-      compareLineIdx < line.length;
-      compareLineIdx++
-    ) {
-      const comparedLine = line[compareLineIdx];
-
-      const intersectionPoint = getIntersectionFromLines(
-        standardLine,
-        comparedLine
-      );
-      if (intersectionPoint) {
-        const [intersectionPointX, intersectionPointY] = intersectionPoint;
-        upLimit = Math.max(upLimit, intersectionPointY);
-        downLimit = Math.min(downLimit, intersectionPointY);
-        leftLimit = Math.min(leftLimit, intersectionPointX);
-        rightLimit = Math.max(rightLimit, intersectionPointX);
-
-        if (!intersectionPoints[intersectionPointY]) {
-          intersectionPoints[intersectionPointY] = [];
-        }
-        intersectionPoints[intersectionPointY].push(intersectionPointX);
-      }
-    }
+(() => {
+  function getIntersectionFromLines([A, B, E], [C, D, F]) {
+    const x = (B * F - E * D) / (A * D - B * C);
+    const y = (E * C - A * F) / (A * D - B * C);
+    return x % 1 == 0 && y % 1 == 0 ? [x, y] : null;
   }
 
-  let emptyCoord = Array.from(
-    { length: rightLimit - leftLimit + 1 },
-    (_, idx) => '.'
-  ).join('');
+  function solution(line) {
+    const answer = [];
 
-  for (let startY = upLimit; startY > downLimit - 1; startY--) {
-    if (!intersectionPoints[startY]) {
-      answer.push(emptyCoord);
-      continue;
+    const intersectionPoints = {};
+
+    let upLimit = Number.MIN_SAFE_INTEGER;
+    let downLimit = Number.MAX_SAFE_INTEGER;
+    let leftLimit = Number.MAX_SAFE_INTEGER;
+    let rightLimit = Number.MIN_SAFE_INTEGER;
+
+    // 교점을 구함
+    for (
+      let standardLineIdx = 0;
+      standardLineIdx < line.length;
+      standardLineIdx++
+    ) {
+      const standardLine = line[standardLineIdx];
+      for (
+        let compareLineIdx = standardLineIdx + 1;
+        compareLineIdx < line.length;
+        compareLineIdx++
+      ) {
+        const comparedLine = line[compareLineIdx];
+
+        const intersectionPoint = getIntersectionFromLines(
+          standardLine,
+          comparedLine
+        );
+        if (intersectionPoint) {
+          // 최대 좌표가 표시되는 위아래, 좌우의 위치를 구함
+          const [intersectionPointX, intersectionPointY] = intersectionPoint;
+          upLimit = Math.max(upLimit, intersectionPointY);
+          downLimit = Math.min(downLimit, intersectionPointY);
+          leftLimit = Math.min(leftLimit, intersectionPointX);
+          rightLimit = Math.max(rightLimit, intersectionPointX);
+
+          // 교점을 객체안에 보관
+          // y좌표가 key, y좌표상에 있는 x좌표를 value array로 둠
+          if (!intersectionPoints[intersectionPointY]) {
+            intersectionPoints[intersectionPointY] = [];
+          }
+          intersectionPoints[intersectionPointY].push(intersectionPointX);
+        }
+      }
     }
 
-    let emptyStr = '';
+    // y좌표상에 x좌표가 없을때 아래를 push함
+    let emptyCoord = Array.from(
+      { length: rightLimit - leftLimit + 1 },
+      (_, idx) => '.'
+    ).join('');
 
-    for (let startX = leftLimit; startX < rightLimit + 1; startX++) {
-      if (intersectionPoints[startY].includes(startX)) {
-        emptyStr += '*';
+    // y좌표를 loop함
+    for (let startY = upLimit; startY > downLimit - 1; startY--) {
+      if (!intersectionPoints[startY]) {
+        answer.push(emptyCoord);
         continue;
       }
-      emptyStr += '.';
+
+      let emptyStr = '';
+
+      // x좌표를 loop함
+      for (let startX = leftLimit; startX < rightLimit + 1; startX++) {
+        if (intersectionPoints[startY].includes(startX)) {
+          emptyStr += '*';
+          continue;
+        }
+        emptyStr += '.';
+      }
+
+      answer.push(emptyStr);
     }
 
-    answer.push(emptyStr);
+    console.log(answer);
+    return answer;
   }
+  const line = [
+    [2, -1, 4],
+    [-2, -1, 4],
+    [0, -1, 1],
+    [5, -8, -12],
+    [5, 8, 12],
+  ];
 
-  console.log(answer);
-  return answer;
-}
-const line = [
-  [2, -1, 4],
-  [-2, -1, 4],
-  [0, -1, 1],
-  [5, -8, -12],
-  [5, 8, 12],
-];
-solution(line);
-/*
-- pesudo code
-0. 교점 arr를 만듬
-1. 두 직선사이의 교점을 구하는 함수 만들기(정수만 arr에 포함)
-2. 교점이 담긴 arr를 y좌표를 기준삼아 큰것에서 작은것순으로 정렬
-3. max Y와 max X를 구함 => for문의 범위를 구하기 위해서
-4. y좌표가 같으면 하나로 묶기!
-5. max y좌표를 시작으로 -1씩 줄여나가면서 그림을 그려가기.
+  // solution(line);
+  /*
+    - pesudo code
+    0. 교점 arr를 만듬
+    1. 두 직선사이의 교점을 구하는 함수 만들기(정수만 arr에 포함)
+    2. 교점이 담긴 arr를 y좌표를 기준삼아 큰것에서 작은것순으로 정렬
+    3. max Y와 max X를 구함 => for문의 범위를 구하기 위해서
+    4. y좌표가 같으면 하나로 묶기!
+    5. max y좌표를 시작으로 -1씩 줄여나가면서 그림을 그려가기.
+    
+    - 한가운데 중점(0,0)을 기준으로 위아래, 좌우의 최대 범위를 구해야한다.
+    - 위아래가 첫번째 포문, 좌우가 두번째 포문이 될듯
+      - max up(첫번째 포문 시작), max down(첫번째 포문 종점), 
+      - max left(두번째 포문 시작), max right(두번째 포문 종점)를 구하기
+    
+    - 할일
+      - 별그리기. 
+        - Y좌표를 KEY로 하고 Y좌표상에 존재하는 X좌표의 위치를 VALUE로 해야겠다
+    */
+})();
 
-- 한가운데 중점(0,0)을 기준으로 위아래, 좌우의 최대 범위를 구해야한다.
-- 위아래가 첫번째 포문, 좌우가 두번째 포문이 될듯
-  - max up(첫번째 포문 시작), max down(첫번째 포문 종점), 
-  - max left(두번째 포문 시작), max right(두번째 포문 종점)를 구하기
-
-- 할일
-  - 별그리기. 
-    - Y좌표를 KEY로 하고 Y좌표상에 존재하는 X좌표의 위치를 VALUE로 해야겠다
-*/
+const arr = [1, 2, 3];
---
+++ 빛의_경로_싸이클.js
@@ -0,0 +1,70 @@
+(() => {
+  // 더 자세한 설명은?
+  // https://velog.io/@seungdeng17/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%B9%9B%EC%9D%98-%EA%B2%BD%EB%A1%9C-%EC%82%AC%EC%9D%B4%ED%81%B4-%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8
+
+  function solution(grid) {
+    const answer = [];
+
+    const dx = [-1, 1, 0, 0];
+    const dy = [0, 0, -1, 1];
+
+    const ch = Array.from({ length: grid.length }, () => []).map(c => {
+      for (let i = 0; i < grid[0].length; i++) c.push([0, 0, 0, 0]);
+      return c;
+    });
+
+    console.log(ch);
+
+    for (let x = 0; x < grid.length; x++) {
+      for (let y = 0; y < grid[0].length; y++) {
+        for (let d = 0; d < dx.length; d++) {
+          if (ch[x][y][d]) continue;
+          const cnt = checker(x, y, d);
+          if (cnt) answer.push(cnt);
+        }
+      }
+    }
+
+    function checker(x, y, d) {
+      let cnt = 0;
+      while (true) {
+        if (ch[x][y][d]) break;
+        console.log(ch);
+        ch[x][y][d] = 1;
+        cnt++;
+
+        x = x + dx[d];
+        y = y + dy[d];
+        if (x < 0) x = grid.length - 1;
+        if (x >= grid.length) x = 0;
+        if (y < 0) y = grid[0].length - 1;
+        if (y >= grid[0].length) y = 0;
+        d = getNextDir(grid[x][y], d);
+      }
+      return cnt;
+    }
+
+    console.log(answer);
+    return answer.sort((a, b) => a - b);
+  }
+
+  function getNextDir(block, dir) {
+    if (block === 'S') return dir;
+    if (block === 'L') return [2, 3, 1, 0][dir];
+    if (block === 'R') return [3, 2, 0, 1][dir];
+  }
+
+  const grid = ['SL', 'LR']; //[16]
+  const grid2 = ['R', 'R'];
+
+  solution(grid);
+  /*
+  pseudo code
+    * while을 써야 할것 같다. 사이클이 끝날때까지 반복해야하므로(BFS? DFS? 큐?)
+    * 1번방향으로 가는 순간이 2번나오는 순간 싸이클이 끝난것이므로 이때 while을 종료
+    * 일단은 1행1열에서 2행 1열로 빛을 쏴볼까? 그렇게 하면 총 4군데에서 빛을 쏴야한다.
+    * grid안에 있는 원소를 일단 나눠야한다. 각각의 grid에 분배되어있는 것 처럼
+    ! 개발자 도구안에 breakpoint를 설정하고 돌려보자
+  
+  */
+})();
