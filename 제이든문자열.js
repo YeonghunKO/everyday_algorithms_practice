4e4e5f396628648281cf2bd31b8dd7b86deca6d0
Sinkyo <yhko1988@gmail.com>
2024-04-01 15:59:13 +0900

    feat : 일단 완성

--- 제이든문자열.js
+++ 제이든문자열.js
@@ -3,7 +3,18 @@
  * "for the last week"	"For The Last Week"
  */
 
-function solution(s) {
-  var answer = "";
-  return answer;
+function solution(words) {
+  words = words.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    if (i === 0 && Number.isInteger(words[i][0])) {
+      continue;
+    }
+
+    words[i] = words[i].replace(words[i][0], words[i][0].toUpperCase());
+  }
+
+  return words.join(" ");
 }
+
+console.log(solution("for the last week"));
