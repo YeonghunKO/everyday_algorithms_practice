e424c3417c367fd7b4eef634b90b7d927cb00154
Sinkyo <yhko1988@gmail.com>
2024-04-03 15:17:24 +0900

    feat : 다른 정답 추가
    - 변수를 줄일 수 있어서 더 깔끔

--- 이진수전환.js
+++ 이진수전환.js
@@ -41,4 +41,14 @@ function solution(s) {
   return [count, deletedZeros];
 }
 
+function solution2(s) {
+  var answer = [0, 0];
+  while (s.length > 1) {
+    answer[0]++;
+    answer[1] += (s.match(/0/g) || []).length;
+    s = s.replace(/0/g, "").length.toString(2);
+  }
+  return answer;
+}
+
 console.log(solution("110010101001"));
