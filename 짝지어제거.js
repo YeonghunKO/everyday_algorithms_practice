fc2f53d2b619f557b606367ac5a6349adcc99bbe
Sinkyo <yhko1988@gmail.com>
2024-04-03 20:11:45 +0900

    feat : pesudo code적기

---
+++ 짝지어제거.js
@@ -0,0 +1,33 @@
+/**
+예를 들어, 문자열 S = baabaa 라면
+
+b aa baa → bb aa → aa →
+
+의 순서로 문자열을 모두 제거할 수 있으므로 1을 반환합니다.
+
+const str = "hellooo world";
+const match = str.match(/(.)\1/);
+
+if (match) {
+    console.log(`Found consecutive same characters: ${match[0]}`);  // 출력: Found consecutive same characters: oo
+} else {
+    console.log("No consecutive same characters found.");
+}
+
+1. while문안에서 regex를 통해 연속된 알파벳이 있는지 찾고 
+    - 있으면 그 알파벳을 "" 로 replace한다.
+        - isSameChar = true
+    - 없으면 isSameChar = false로 하고 빠져나온다.
+
+
+ */
+
+function solution(string) {
+  let isSameChar = true;
+
+  while (isSameChar) {
+    if (string) {
+    }
+    // string.
+  }
+}
