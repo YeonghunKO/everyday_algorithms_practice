870ca1c46e7c6e7dcbbf2f134a98e4bf1968c2dc
Sinkyo <yhko1988@gmail.com>
2024-04-04 10:45:10 +0900

    feat : 주석 추가

--- 끝말잇기.js
+++ 끝말잇기.js
@@ -2,6 +2,11 @@
 ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]	[3,3]
 ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]	[0,0]
 ["hello", "one", "even", "never", "now", "world", "draw"]
+
+몇번째 사람이 몇번째에 탈락했는지.
+
+1. 이미 나온단어 말하며 탈락
+2. 앞의 단어의 끝으로 시작 안되면 탈락
  */
 
 function solution(n, words) {}
