169e31f80f820108ae3a1931f005509e9655058a
Sinkyo <yhko1988@gmail.com>
2024-04-04 11:56:40 +0900

    feat : 정답 추가

--- 끝말잇기.js
+++ 끝말잇기.js
@@ -41,7 +41,24 @@ function solution(n, words) {
 
   console.log("nth", nth);
 
-  return nth % n ? [nth & n, n + 1] : [nth / n, nth / n];
+  var no = (nth % n) + 1;
+  var turn = Math.floor(nth / n) + 1;
+
+  return [no, turn];
+}
+
+function solution2(n, words) {
+  let answer = 0;
+  words.reduce((prev, now, idx) => {
+    answer =
+      answer ||
+      (words.slice(0, idx).indexOf(now) !== -1 || prev !== now[0]
+        ? idx
+        : answer);
+    return now[now.length - 1];
+  }, "");
+
+  return answer ? [(answer % n) + 1, Math.floor(answer / n) + 1] : [0, 0];
 }
 
 const n = 2;
