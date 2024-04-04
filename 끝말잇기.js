9a9daa87aa3551a8914b7c7e5f3071480a52623a
Sinkyo <yhko1988@gmail.com>
2024-04-04 10:15:57 +0900

    feat : 끝말잇기 init

---
+++ 끝말잇기.js
@@ -0,0 +1,7 @@
+/**
+["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]	[3,3]
+["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]	[0,0]
+["hello", "one", "even", "never", "now", "world", "draw"]
+ */
+
+function solution(n, words) {}
