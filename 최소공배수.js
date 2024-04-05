c1500fb5cf8972e12f308b40884f7fdccb26c60e
Sinkyo <yhko1988@gmail.com>
2024-04-05 11:42:56 +0900

    feat : gcd 테스트

--- 최소공배수.js
+++ 최소공배수.js
@@ -10,8 +10,13 @@ function gcd(a, b) {
   return a % b ? gcd(b, a % b) : b;
 }
 
+/**
+ * 6,2
+ */
+
 // 아래는 테스트로 출력해 보기 위한 코드입니다.
 console.log(nlcm([2, 6, 8, 14]));
+// console.log(gcd(6, 8));
 // console.log(nlcm([12, 78, 35, 20, 502, 14, 52, 62]))
 // console.log(nlcm([1, 28, 3, 33, 50, 14, 52, 62]))
 // console.log(nlcm([33,11,24,17,82,193]))
