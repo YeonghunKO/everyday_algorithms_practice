0cc462d064414c7c61efe520c705287c33a267eb
Sinkyo <yhko1988@gmail.com>
2024-04-07 08:35:28 +0900

    feat : 귤고르기 완성!

--- 귤고르기.js
+++ 귤고르기.js
@@ -17,7 +17,7 @@ pseudo code
 3. k개 만큼 골랐으면 break하고 answer를 리턴
  */
 
-function solution(k, tangerine) {
+function solution(targetCount, tangerine) {
   let answer = 0;
   const tDict = {};
 
@@ -30,6 +30,16 @@ function solution(k, tangerine) {
   });
 
   const sortedDict = Object.values(tDict).sort((a, b) => b - a);
+
+  for (const count of sortedDict) {
+    answer++;
+    if (targetCount > count) {
+      targetCount -= count;
+    } else {
+      break;
+    }
+  }
+  return answer;
 }
 
 solution(6, [1, 3, 2, 5, 4, 5, 2, 3]);
