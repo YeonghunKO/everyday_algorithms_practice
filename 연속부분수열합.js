58497120649c2cc7b687940aa5fa9eaf695aa492
Sinkyo <yhko1988@gmail.com>
2024-04-08 20:14:37 +0900

    feat : 답지를 봄..

--- 연속부분수열합.js
+++ 연속부분수열합.js
@@ -6,5 +6,26 @@
 길이가 5인 연속 부분 수열로부터 [22] 한 가지의 합이 나올 수 있습니다.
 이들 중 중복되는 값을 제외하면 다음과 같은 18가지의 수들을 얻습니다.
 [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 15, 16, 17, 18, 20, 21, 22]
+
+[7, 9, 1, 1, 4, 7, 9, 1, 1, 4]
+pseudo code
+1. addSum 재귀함수를 while문에서 돌리기
+2. 
  */
-function solution(params) {}
+
+const elements = [7, 9, 1, 1, 4]; // 18
+
+function solution(elements) {
+  const circular = elements.concat(elements);
+  const set = new Set();
+  for (let i = 0; i < elements.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < elements.length; j++) {
+      sum += circular[i + j];
+      set.add(sum);
+    }
+  }
+  return set.size;
+}
+
+solution(elements);
