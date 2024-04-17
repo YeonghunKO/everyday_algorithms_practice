bb0c6b0aae0c2c1098086f2793f1a0dfddb16c82
Sinkyo <yhko1988@gmail.com>
2024-04-17 11:33:04 +0900

    feat : Init

---
+++ 할인행사.js
@@ -0,0 +1,16 @@
+/**
+ *
+ *
+ * want ["banana", "apple", "rice", "pork", "pot"]
+ * number [3, 2, 2, 2, 1]
+ * discount ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"]
+ * result 3
+ *
+ *
+ * ["apple"]
+ * [10]
+ * ["banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"]
+ * 0
+ */
+
+function solution(want, number, discount) {}
