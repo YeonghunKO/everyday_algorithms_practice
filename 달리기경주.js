40fbc3bd73275336c8ce6fb01a50ba3a950aba28
Sinkyo <yhko1988@gmail.com>
2024-04-18 16:48:06 +0900

    feat : init

---
+++ 달리기경주.js
@@ -0,0 +1,23 @@
+/**
+ * players	callings	result
+["mumu", "soe", "poe", "kai", "mine"]	
+
+["kai", "kai", "mine", "mine"]	
+
+["mumu", "kai", "mine", "soe", "poe"]
+
+pseudo code
+1. callings를 순회하기
+    - 각각의 이름이 몇번 불렸는지 이름과 횟수 매핑하기.
+2. 불려진 선수가 최종적으로 위치할 idx가 어딘지 구하기
+3. swapElements를 이용해 불려진 이름이 idx에 위치한 선수와 swap되게 하기
+
+ */
+
+const swapElements = (array, index1, index2) => {
+  let temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+};
+
+function solution(players, callings) {}
